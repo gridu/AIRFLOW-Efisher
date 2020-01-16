31bc10ead00741165500a5d34d28e6485275cd2e
jagular-spb <jagular.spb@gmail.com>
2020-01-16 17:04:26 +0300

    Step-01

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -1,17 +1,17 @@
-"""
+
 import airflow
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-"""
+
 from datetime import datetime, timedelta
 
 concurrency = 2
 catchup = False
 
 config = {
-    'dag_id_1': {'schedule_interval': timedelta(minutes=15), "start_date": datetime.fromisoformat('2020-01-16'), "max_active_runs": 1},
-    'dag_id_2': {'schedule_interval': timedelta(minutes=20), "start_date": datetime.fromisoformat('2020-01-16'), "max_active_runs": 1},
-    'dag_id_3': {'schedule_interval': timedelta(minutes=25), "start_date": datetime.fromisoformat('2020-01-16'), "max_active_runs": 1}
+    'dag_id_1': {'schedule_interval': timedelta(minutes=15), 'start_date': datetime.fromisoformat('2020-01-16'), 'max_active_runs': 1},
+    'dag_id_2': {'schedule_interval': timedelta(minutes=20), 'start_date': datetime.fromisoformat('2020-01-16'), 'max_active_runs': 1},
+    'dag_id_3': {'schedule_interval': timedelta(minutes=25), 'start_date': datetime.fromisoformat('2020-01-16'), 'max_active_runs': 1}
 }
 
 
@@ -19,14 +19,14 @@ config = {
 
 for dict in config:
     print("%s" % dict)
-"""
-    with DAG(dag_id=dict.dag_id,
-             schedule_interval=dict.schedule_interval,
-             start_date=dict.start_date,
-             max_active_runs=dict.max_active_runs,
+
+    with DAG(dag_id=dict,
+             schedule_interval=config[dict]['schedule_interval'],
+             start_date=config[dict]['start_date'],
+             max_active_runs=config[dict]['max_active_runs'],
              concurrency=concurrency,
              catchup=catchup
              ) as dag:
         DummyOperator(task_id='dummy-task',dag=dag)
 
-"""
+
