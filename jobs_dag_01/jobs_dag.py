f1297626de9685be352fc12f216f49e857a158c4
jagular-spb <jagular.spb@gmail.com>
2020-01-16 15:59:07 +0300

    Step-01

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -1,9 +1,29 @@
+import airflow
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 
+concurrency = 2
+catchup = False
+
 config = {
-    'dag_id_1': {'schedule_interval': timedelta(minutes=15), "start_date": datetime(2020, 01, 11)},
-    'dag_id_2': {'schedule_interval': timedelta(minutes=20), "start_date": datetime(2020, 01, 11)},
-    'dag_id_3': {'schedule_interval': timedelta(minutes=25), "start_date": datetime(2020, 01, 11)}
+    'dag_id_1': {'schedule_interval': timedelta(minutes=15), "start_date": datetime(2020, 01, 11), "max_active_runs": 1},
+    'dag_id_2': {'schedule_interval': timedelta(minutes=20), "start_date": datetime(2020, 01, 11), "max_active_runs": 1},
+    'dag_id_3': {'schedule_interval': timedelta(minutes=25), "start_date": datetime(2020, 01, 11), "max_active_runs": 1}
 }
+
+
+
+
+for dict in config:
+    print("%s" % dict)
+    with DAG(dag_id=dict.dag_id,
+             schedule_interval=dict.schedule_interval,
+             start_date=dict.start_date,
+             max_active_runs=dict.max_active_runs,
+             concurrency=concurrency,
+             catchup=catchup
+             ) as dag:
+        DummyOperator(task_id='dummy-task',dag=dag)
+
+
