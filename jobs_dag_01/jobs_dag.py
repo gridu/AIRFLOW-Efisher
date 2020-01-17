c6b8e7ff2e722b3b42d2d08a7df88000580f2f20
Egor A. Fisher <efisher@griddynamics.com>
2020-01-17 16:53:29 +0300

    test

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -9,18 +9,22 @@ catchup = False
 
 config = {
     'dag_id_1': {'schedule_interval': timedelta(minutes=15), 'start_date': datetime(2020, 1, 16), 'max_active_runs': 1},
-    'dag_id_2': {'schedule_interval': timedelta(minutes=20), 'start_date': airflow.utils.dates.days_ago(1),
-                 'max_active_runs': 1},
-    'dag_id_3': {'schedule_interval': timedelta(minutes=25), 'start_date': airflow.utils.dates.days_ago(2),
-                 'max_active_runs': 1}
+    'dag_id_20': {'schedule_interval': timedelta(minutes=15), 'start_date': datetime(2020, 1, 16), 'max_active_runs': 1}
 }
 
+"""
+'dag_id_2': {'schedule_interval': timedelta(minutes=20), 'start_date': airflow.utils.dates.days_ago(1),
+             'max_active_runs': 1},
+'dag_id_3': {'schedule_interval': timedelta(minutes=25), 'start_date': airflow.utils.dates.days_ago(2),
+             'max_active_runs': 1}
+"""
+
 for dict in config:
-    print("%s" % dict)
 
     with DAG(dag_id=dict, schedule_interval=config[dict]['schedule_interval'], start_date=config[dict]['start_date'],
              max_active_runs=config[dict]['max_active_runs'], dagrun_timeout=timedelta(minutes=10),
              concurrency=concurrency, catchup=catchup) as dag:
         DummyOperator(task_id='dummy-task', dag=dag)
+        print("%s" % config[dict]['start_date'])
 
 """ test 2 """
