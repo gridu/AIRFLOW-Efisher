ba1bb52d5038dcb6c7f21f1f5e20d0213800f4eb
Egor A. Fisher <efisher@griddynamics.com>
2020-01-18 17:45:04 +0300

    test

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -14,14 +14,19 @@ config = {
                  'max_active_runs': 1},
     'dag_id_3': {'schedule_interval': timedelta(minutes=25), 'start_date': airflow.utils.dates.days_ago(2),
                  'max_active_runs': 1},
-
     'dag_id_20': {'schedule_interval': timedelta(minutes=15), 'start_date': datetime(2020, 1, 16), 'max_active_runs': 1}
 }
 
 for dict in config:
-    with DAG(dag_id=dict, schedule_interval=config[dict]['schedule_interval'], start_date=config[dict]['start_date'],
-             max_active_runs=config[dict]['max_active_runs'], dagrun_timeout=timedelta(minutes=10),
-             concurrency=concurrency, catchup=catchup) as dag:
+    args = {
+        'owner': 'airflow',
+        'start_date': config[dict]['start_date'],
+        'max_active_runs': config[dict]['max_active_runs'],
+        'dagrun_timeout': timedelta(minutes=10),
+        'concurrency': concurrency,
+        'catchup': catchup
+    }
+    with DAG(dag_id=dict, default_args=args, schedule_interval=config[dict]['schedule_interval']) as dag:
         dop0 = DummyOperator(task_id='dummy-task-' + dict)
         dop1 = BashOperator(task_id='dummy-sub-task-' + dict, bash_command='echo `date`')
         dop1.set_upstream(dop0)
