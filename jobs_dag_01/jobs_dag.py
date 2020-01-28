d70780c9179bca924312ec594a72e053e0765596
Egor A. Fisher <efisher@griddynamics.com>
2020-01-28 17:15:42 +0300

    step-02

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -20,11 +20,11 @@ config = {
 }
 
 
-def print_to_log(ds, **kwargs):
+def print_to_log(ti,**kwargs):
 
     dag_id = "1234"
     pprint(kwargs)
-    print("ds: %s" % ds)
+    print("ds: %s" % ti.dag_id)
     database = kwargs['database']
     table = kwargs['table']
     print(" %s start processing tables in database: %s.%s" % (dag_id, database, table))
