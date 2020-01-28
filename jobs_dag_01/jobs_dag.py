98d4baa543a13b9b3dc9063befb98c42418006db
Egor A. Fisher <efisher@griddynamics.com>
2020-01-28 14:47:14 +0300

    step-02

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -1,21 +1,29 @@
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
 from datetime import datetime, timedelta
 
 concurrency = 2
 catchup = False
 
 config = {
-    'dag_id_1': {'schedule_interval': timedelta(minutes=45), 'start_date': datetime(2020, 1, 18), 'max_active_runs': 2},
-
+    'dag_id_1': {'schedule_interval': timedelta(minutes=45), 'start_date': datetime(2020, 1, 18), 'max_active_runs': 2,
+                 "table_name": "table_num_1"},
     'dag_id_2': {'schedule_interval': timedelta(minutes=40), 'start_date': airflow.utils.dates.days_ago(1),
-                 'max_active_runs': 3},
+                 'max_active_runs': 3, "table_name": "table_num_2"},
     'dag_id_3': {'schedule_interval': timedelta(minutes=55), 'start_date': airflow.utils.dates.days_ago(1),
-                 'max_active_runs': 1},
-    'dag_id_20': {'schedule_interval': timedelta(minutes=35), 'start_date': datetime(2020, 1, 18), 'max_active_runs': 1}
+                 'max_active_runs': 1, "table_name": "table_num_3"}
 }
+
+
+def print_to_log(**kwargs):
+    dag_id = kwargs['dag_id']
+    database = kwargs['database']
+    print(" %s start processing tables in database: %s" % (dag_id, database))
+    return 'Whatever you return gets printed in the logs'
+
 
 for dict in config:
     args = {
@@ -24,10 +32,16 @@ for dict in config:
         'max_active_runs': config[dict]['max_active_runs'],
         'dagrun_timeout': timedelta(minutes=10),
         'concurrency': concurrency,
-        'catchup': catchup
+        'catchup': catchup,
+        'database': 'our_test_db'
     }
     with DAG(dag_id=dict, default_args=args, schedule_interval=config[dict]['schedule_interval']) as dag:
-        dop0 = DummyOperator(task_id='dummy-task-' + dict)
+
+        dop0 = PythonOperator(task_id='python-task-' + dict,
+                              provide_context=True,
+                              python_callable=print_to_log
+                              )
+
         dop1 = BashOperator(task_id='dummy-sub-task-' + dict, bash_command='echo `date`')
         dop1.set_upstream(dop0)
 
