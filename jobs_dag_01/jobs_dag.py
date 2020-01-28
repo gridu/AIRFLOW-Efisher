77fd01627ddf3723e6c1bd75be9f310960ffa93d
Egor A. Fisher <efisher@griddynamics.com>
2020-01-28 17:30:29 +0300

    step-02

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -1,6 +1,7 @@
 import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 from datetime import datetime, timedelta
 
