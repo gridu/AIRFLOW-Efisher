064e71b7739136634e5cd2ac178a9d9a9e5a8bfb
Egor A. Fisher <efisher@griddynamics.com>
2020-02-03 13:39:21 +0300

    step-04

--- jobs_dag_01/trigger_dag.py
+++ jobs_dag_01/trigger_dag.py
@@ -14,8 +14,11 @@ catchup = False
 database = "our_test_db"
 
 config = {
-    'triggered_dag_id_1': {'schedule_interval': timedelta(minutes=45), 'start_date': datetime(2020, 2, 3), 'max_active_runs': 1
-    }
+    'triggered_dag_id_1': {'schedule_interval': timedelta(minutes=45),
+                           'start_date': datetime(2020, 2, 3),
+                           'max_active_runs': 1
+                           }
+}
 
 
 for dict in config:
@@ -23,14 +26,20 @@ for dict in config:
         'owner': 'airflow',
         'start_date': config[dict]['start_date'],
         'dagrun_timeout': timedelta(minutes=10),
-        'concurrency': concurrency,
-        'catchup': catchup
+        'retries': 3,
+        'retry_delay': datetime.timedelta(hours=5)
+        'poke_interval': 30
     }
 
-    with DAG(dag_id = dict, default_args = args, schedule_interval = config[dict]['schedule_interval'],
-         start_date = config[dict]['start_date'],
-         concurrency= concurrency,
-         max_active_runs = config[dict]['max_active_runs']) as dag:
+    with DAG(dag_id=dict, default_args=args, schedule_interval=config[dict]['schedule_interval'],
+         start_date =config[dict]['start_date'],
+         concurrency=concurrency,
+         max_active_runs =config[dict]['max_active_runs']) as dag:
+
+        sensor000 = FileSensor(task_id="file_sensor_task",
+                                poke_interval=default_args['poke_interval'],
+                                fs_conn_id="fs_default",
+                                filepath="/tmp/trigger_it")
 
 
     if dag:
