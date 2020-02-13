220126cacae61430aec00346298b3dd85b92e7b5
Egor A. Fisher <efisher@griddynamics.com>
2020-02-14 00:20:22 +0300

    step-05

--- jobs_dag_01/trigger_dag.py
+++ jobs_dag_01/trigger_dag.py
@@ -25,7 +25,7 @@ config = {
 
 def get_date(execution_date,**kwargs):
 
-    next_execution_date = {execution_date
+    next_execution_date = execution_date
     print ("{}->{}".format(execution_date,next_execution_date))
     return next_execution_date
 
