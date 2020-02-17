4e21cdcfcb08af0339c17b4d2346ce4147528e96
Egor A. Fisher <efisher@griddynamics.com>
2020-02-17 13:30:40 +0300

    step-05-2

--- jobs_dag_01/its_sub_dag.py
+++ jobs_dag_01/its_sub_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.subdag_operator import SubDagOperator
 
 from datetime import datetime, timezone, timedelta
 
@@ -8,10 +7,10 @@ concurrency = 4
 catchup = False
 
 config = {
-    'sub_dag_id_1': {'schedule_interval': timedelta(minutes=45),
-                     'start_date': datetime(2020, 2, 3, 9, 0, 0, 0, tzinfo=timezone.utc),
-                     'max_active_runs': 1
-                     }
+    'dag_with_subdag_id_1': {'schedule_interval': timedelta(minutes=45),
+                             'start_date': datetime(2020, 2, 3, 9, 0, 0, 0, tzinfo=timezone.utc),
+                             'max_active_runs': 1
+                             }
 }
 
 
@@ -22,13 +21,31 @@ def load_subdag(parent_dag_name, child_dag_name, args):
     )
     with dag_subdag:
         t = DummyOperator(
-            task_id='load_subdag_1',
+            task_id='load_subdag_{0}'.format(child_dag_name),
             default_args=args,
             dag=dag_subdag,
         )
 
-    return dag_subdag
+        external_check = ExternalTaskSensor(
+            task_id='ext_check_task_{0}'.format(child_dag_name),
+            external_dag_id='dag_id_1',
+            external_task_id=None,
+            execution_date_fn=get_date,
+            mode="reschedule",
+            default_args=args,
+            allowed_states=['success'],
+            dag=dag_subdag,
+        )
 
+        print_result = PythonOperator(task_id='print_result_task_{0}'.format(child_dag_name),
+                                      provide_context=True,
+                                      python_callable=print_result,
+                                      default_args=args,
+                                      dag=dag_subdag
+                                      )
+
+
+return dag_subdag
 
 for dict in config:
     args = {
@@ -52,30 +69,6 @@ for dict in config:
             subdag=load_subdag('{0}'.format(dict), '{0}_subdag'.format(dict), args),
             task_id='{0}_subdag'.format(dict),
         )
-
-        """
-        sensor000 = FileSensor(task_id="file_sensor_task",
-                               fs_conn_id="fs_default",
-                               mode="reschedule",
-                               filepath=trigger_path)
-
-        trigger_on_000 = TriggerDagRunOperator(task_id="trigger_on", trigger_dag_id="dag_id_1", execution_date='{{ execution_date }}')
-        trigger_off_000 = BashOperator(task_id='trigger_off', bash_command='rm -f {{ trigger_path }}')
-
-        external_check = ExternalTaskSensor(
-            task_id='check_dag_id_1',
-            external_dag_id='dag_id_1',
-            external_task_id=None,
-            execution_date_fn=get_date,
-            mode="reschedule",
-            allowed_states=['success']
-        )
-
-
-        trigger_on_000.set_upstream(sensor000)
-        external_check.set_upstream(trigger_on_000)
-        trigger_off_000.set_upstream(external_check)
-        """
 
     if dag:
         globals()[dict] = dag
