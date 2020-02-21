63a123290d78149b812ea15f14ee84047176ff80
Egor A. Fisher <efisher@griddynamics.com>
2020-02-21 12:37:32 +0300

    step-05-2

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -29,7 +29,7 @@ def print_to_log(ti, **kwargs):
     return "[print_to_log] end"
 
 def report_result(ti, **kwargs):
-        ti.xcom_push(key='all_done', value=True)
+        ti.xcom_push(key='all_done', value="{{ run_id }} ended")
         print('We are done')
 
 def check_table_exist(ti, **kwargs):
@@ -72,41 +72,41 @@ for dict in config:
                               python_callable=print_to_log
                               )
         dop001 = BashOperator(task_id='bash-sub-task-' + dict, bash_command='echo "Running under user: ${USER}"')
-        dop001.set_upstream(dop00)
+        #dop001.set_upstream(dop00)
 
         dop01 = PythonOperator(task_id='check-table-task-' + dict,
                                provide_context=True,
                                python_callable=check_table_exist
                                )
-        dop01.set_upstream(dop001)
+        #dop01.set_upstream(dop001)
 
         dop02 = BranchPythonOperator(task_id='create_or_not_table' + dict,
                                provide_context=True,
                                python_callable=create_or_not_table
                                )
-        dop02.set_upstream(dop01)
+        #dop02.set_upstream(dop01)
 
         dop03 = DummyOperator(task_id='create_table')
-        dop03.set_upstream(dop02)
+        #dop03.set_upstream(dop02)
         dop04 = DummyOperator(task_id='skip_table_creation')
-        dop04.set_upstream(dop02)
+        #dop04.set_upstream(dop02)
 
         dop05 = DummyOperator(task_id='insert-new-row-' + dict, trigger_rule='none_failed')
-        dop05.set_upstream([dop03, dop04])
+        #dop05.set_upstream([dop03, dop04])
 
         dop06 = DummyOperator(task_id='query-the-table-' + dict)
-        dop06.set_upstream(dop05)
+        #dop06.set_upstream(dop05)
 
         dop07 = PythonOperator(task_id='last-task',
                                provide_context=True,
                                python_callable=report_result
                                )
-        dop07.set_upstream(dop06)
+        #dop07.set_upstream(dop06)
 
+        dop00 >> dop001 >> dop01>> dop02 >> [dop03, dop04] >> dop05 >> dop06 >> dop07
 
     if dag:
         globals()[dict] = dag
     else:
         print("Finished")
 
-""" test 2 """
