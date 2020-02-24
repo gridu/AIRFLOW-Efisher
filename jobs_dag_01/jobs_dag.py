d5c33da3022072e2a149b9e1fd860a3d501b5ef5
Egor A. Fisher <efisher@griddynamics.com>
2020-02-24 13:24:44 +0300

    step-06

--- jobs_dag_01/jobs_dag.py
+++ jobs_dag_01/jobs_dag.py
@@ -113,8 +113,6 @@ for dict in config:
 
         dop04 = DummyOperator(task_id='skip_table_creation')
 
-        reported_user = ti.xcom_pull(task_ids='report-user-sub-task-dag_id_1')
-
         dop05 = PostgresOperator(task_id='insert-new-row-' + dict,
                                  trigger_rule='none_failed',
                                  sql='''INSERT INTO {} VALUES(%s, %s, %s);'''.format(config[dict]['table_name']),
